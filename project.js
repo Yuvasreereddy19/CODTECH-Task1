06f1db0fef4c184db1acacfa6a79c37e535542ff
Yuvasreereddy19 <yuvasreereddyjanga@gmail.com>
2024-07-19 22:08:06 +0530

    Create project.js

---
+++ project.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Project.css'; 
+
+const Project = () => {
+  return (
+    <div className="project">
+        <h1>PROJECTS :</h1>
+     <p>1.Community service project on higher education(non technical)</p>
+    <p>2.Mini Project on diagonstic report in virtual format using Vuforia framework and unity hub in augmented reailty(technical)</p>
+    </div>
+  );
+};
+
+export default Project;
