36aef971d616b429d87170cc052c4e57568749ed
Yuvasreereddy19 <yuvasreereddyjanga@gmail.com>
2024-07-19 22:19:42 +0530

    Update project.js

--- project.js
+++ project.js
@@ -12,3 +12,29 @@ const Project = () => {
 };
 
 export default Project;
+
+
+
+
+\*project.css*\
+.project {
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    padding: 16px;
+    margin: 16px;
+    background-color: #f9f9f9;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    text-align: center;
+  }
+  
+  .project p {
+    font-size: 1.8em;
+    color: #333;
+    margin: 0;
+    padding: 0;
+  }
+  
+  .project p:hover {
+    color: #007bff;
+  }
+  
